87d1a6035d2cfa7d4e6d12deb5845b843fbf4317
Sorataro <sora.honda71@icloud.com>
2023-08-24 13:14:11 +0900

    add: SAKURUG2daysIS

---
+++ src/main/java/com/todo/app/mapper/TodoMapper.java
@@ -0,0 +1,21 @@
+package com.todo.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.todo.app.entity.Todo;
+
+@Mapper
+public interface TodoMapper {
+
+//    public List<Todo> selectAll();
+	
+    public List<Todo> selectTime();
+    public List<Todo> selectTitle();
+    public List<Todo> selectComplete();
+    public void add(Todo todo);
+    public void update(Todo todo);
+    public void delete();
+}
+
